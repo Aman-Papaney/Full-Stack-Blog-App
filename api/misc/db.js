d46659824e702d03c032873c71cba77d98eb7528
Aman-Papaney <91885201+Aman-Papaney@users.noreply.github.com>
2025-04-29 14:54:28 +0530

    Update db.js

--- api/misc/db.js
+++ api/misc/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = () => {
 	mongoose
-		.connect("mongodb+srv://greenmilkweed:AGPn6x6AmFio108n@cluster1.ju0kk.mongodb.net/chronicles?retryWrites=true&w=majority&appName=Cluster1")
+		.connect(process.env.MONGO_URI)
 		.then(() => {
 			console.log("Database Connection Successfull")
 		})
